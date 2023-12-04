016f4453cb11fd4fe58de68546fe336e4a070e59
Purvik Rana <purvik1239@gmail.com>
2023-12-04 16:02:13 +0530

    Initial Setup in the System

--- lib/network/interceptors/logger_interceptor.dart
+++ lib/network/interceptors/logger_interceptor.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
 
 class LoggerInterceptor extends Interceptor {
-  Logger logger = Logger(
+  Logger log = Logger(
     printer: PrettyPrinter(
       methodCount: 0,
       errorMethodCount: 5,
@@ -17,21 +17,21 @@ class LoggerInterceptor extends Interceptor {
   void onError(DioError err, ErrorInterceptorHandler handler) {
     final options = err.requestOptions;
     final requestPath = '${options.baseUrl}${options.path}';
-    logger.e('${options.method} request => $requestPath');
-    logger.d('Error: ${err.error}, Message: ${err.message}');
+    log.e('${options.method} request => $requestPath');
+    log.d('Error: ${err.error}, Message: ${err.message}');
     return super.onError(err, handler);
   }
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     final requestPath = '${options.baseUrl}${options.path}';
-    logger.i('${options.method} request => $requestPath');
+    log.i('${options.method} request => $requestPath');
     return super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    logger.d('StatusCode: ${response.statusCode}, Data: ${response.data}');
+    log.d('StatusCode: ${response.statusCode}, Data: ${response.data}');
     return super.onResponse(response, handler);
   }
 }
